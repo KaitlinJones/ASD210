c3d652f0813c48aec8390e661a457efb8eb083e3
Kaitlin Jones <kaitlinjones@joneskaitlin.com>
2022-01-08 23:48:06 -0800

    project 9

--- Week1Part2HandsOn.py
+++ Week1Part2HandsOn.py
@@ -3,7 +3,7 @@ Week 1 Part 2 Hands On
 Kaitlin Jones
 1/7/22
 """
-"""
+
 # Page 62 project 6
 
 print("Program to calculate an object's momentum and kenetic energy.\n")
@@ -38,12 +38,12 @@ print("\nYour regular pay is: $", round(regPay, 2))
 print("\nYour overtime pay is: $", round(overTimePay, 2))
 print("\nYour total pay is: $", round(totalPay, 2))
 
-"""
+
 # Page 100 Project 8
 
 # Take input from user
-smallNum =(int(input("\nPlease enter a number: ")))
-largeNum =(int(input("\nPlease enter a number that is smaller than the first number that you entered: ")))
+smallNum = int(input("\nPlease enter a number: "))
+largeNum = int(input("Please enter a number that is smaller than the first number that you entered: "))
 
 # calculate using Euclid's algorithm
 while smallNum > 0:
@@ -52,3 +52,31 @@ while smallNum > 0:
     smallNum = remainder
 
 print("The greatest common denominator is ", largeNum)
+
+
+# Page 101 Project 9
+
+theSum = 0    #set sum to 0
+count = 0     # set count to 0
+
+# Take input from user until enter is pressed
+
+while True:
+    number = input("Please enter a number or press 'enter' to quit: ")
+    if number == "":
+        break
+    
+# Calculate and print sum
+
+    theSum += float(number)
+    count += 1
+
+print("The sum is : ", theSum)
+
+# Calculate and print average
+    
+if count > 0:
+    print("The average is: ", theSum / count)
+
+
+
