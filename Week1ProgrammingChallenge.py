d756f1723f947eea3968bb1685710df329469358
Kaitlin Jones <kaitlinjones@joneskaitlin.com>
2022-01-15 01:45:32 -0800

    add instructions

--- Week1ProgrammingChallenge.py
+++ Week1ProgrammingChallenge.py
@@ -1,4 +1,16 @@
+"""Generate 5 random numbers integer (whole) and store them in 5 separate variables.
+Display some instructions to user, on what is expected – i.e. This is a digital lottery for 5 numbers ranging from 1 to 99. You have a chance to win it is you guess all 5 numbers correctly.
+Store loop number, i.e. you will need to know if that was 1st loop or 2nd.. etc. 5th.
+Start most outer loop. Check what loop number is running.  Use SWIRCH CASE control to  assign maxGuesses variable a corresponding value from those you  generated in the step 1
+Start the INNER LOOP – same as given in the BASE CODE, but limit it to 10 attempts.
+If user gussed the number correctly during the INNER LOOP execution – REMEMBER the WINNING NUMBER, then  move to NEXT OUTER LOOP CYCLE
+IF user exhausted all 10 attempts and did not guess the number correctly - REMEMBER the ZERO,  then  move to NEXT OUTER LOOP CYCLE.
+When all cycles completed – you should have your WINNING numbers verified and ready to print."""
+
+
 import random
+
+print("Welcome to the lucky lotto! You will have 10 chances per number to guess 5 random numbers from 1 - 99")
 smaller = int(input("Enter the smaller number: "))
 larger = int(input("Enter the larger number: "))
 myNumber = random.randint(smaller, larger)
