227e49d63c83a604992abb5fc8caca2f97fadcb8
Kaitlin Jones <kaitlinjones@joneskaitlin.com>
2022-01-17 12:19:30 -0800

    shift left/right scripts

---
+++ shiftLeft.py
@@ -0,0 +1,4 @@
+
+bit_string = input("Please enter a string of bits: ")
+left = bit_string[1:] + bit_string[0]
+print(left)
---
+++ shiftRight.py
@@ -0,0 +1,4 @@
+
+bit_string = input("\nPlease enter a string of bits: ")
+right = bit_string[-1] + bit_string[:-1]
+print(right)
