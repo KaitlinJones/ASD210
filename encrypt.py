93933f65b6545fd948eaa3e6144ad4223c5e9fab
Kaitlin Jones <kaitlinjones@joneskaitlin.com>
2022-01-17 11:18:18 -0800

    week 2 hands on encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -1,13 +1,14 @@
+"""
+ASD 210 Fundamentals of Python and Best Practices - Nathan Kilgore
+Kaitlin Jones
+Week 2 Hands On 1
+Jan 15 2021
+"""
 
+plainText = str(input("\nPlease enter a message: "))
+distance = int(input("Please enter the distance: "))
 
-plainText = input("Please enter a message: ")
-distance = input("Please enter the distance: ")
-code = ""
-for ch in plainText:
-    ordVal = ord(ch)
-    cipherVal = ordVal + int(distance)
-    if cipherVal > 127:
-        cipherVal = distance - (127 - ordVal + 1)
-    code += chr(cipherVal)
-print(code)
- 
+for i in range(len(plainText)):
+    print(chr((ord(plainText[i]) + distance)), end = "")
+print("\n")
+    
