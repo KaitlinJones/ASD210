e87d07dbe2648079b95545e6c422218565365284
Kaitlin Jones <kaitlinjones@joneskaitlin.com>
2022-01-18 15:02:41 -0800

    functions

---
+++ functions.py
@@ -0,0 +1,17 @@
+"""def odd(x):
+    if x % 2 == 1:
+        return True
+    else:
+        return False
+x = int(input("enter an integer: "))
+
+print(odd(x))"""
+
+def greet(name):
+    if name == "Claire":
+        return "who do you think you are?"
+    return "Hey there, Welcome, " + name
+  
+
+
+print(greet("Claire"))
