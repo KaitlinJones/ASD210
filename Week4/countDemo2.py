bbc5cc9266998188a4baf87b6a42e5c599bf53c0
Kaitlin Jones <kaitlinjones@joneskaitlin.com>
2022-01-25 00:29:05 -0800

    trying random

--- Week4/countDemo2.py
+++ Week4/countDemo2.py
@@ -1,3 +1,4 @@
+from ast import Store
 from breezypythongui import EasyFrame
 import random
 
@@ -6,35 +7,44 @@ class counterDemo(EasyFrame):
     def __init__(self):
         EasyFrame.__init__(self, title = "Guessing Game!")
         self.setSize(300, 300)
-        self.count = 0 
-        self.myNumber = random.randint(1, 100)
-        guess = "Is the number " + str(self.myNumber) + "?"
+        self.count = 1
+        self.lowerbound = 0
+        self.upperbound = 0
+        self.compNum = random.randint(1, 100)
+        self.storeComp = []
+        guess = "Is the number " + str(self.compNum) + "?"
         self.label = self.addLabel(text = guess, row = 0, column = 0, sticky = "NSEW", columnspan = 4)
+        for i in self.storeComp:
+            self.storeComp.append(self.compNum)
 
         self.addButton(text = "Too Large", row = 1, column = 0, command = self.tooLarge)
         self.addButton(text = "Too Small", row = 1, column = 1, command = self.tooSmall)
-        self.addButton(text = "Correct", row = 1, column = 2, command = self.correct)
-        self.addButton(text = "New game", row = 1, column = 3, command = self.reset)
+        self.addButton(text = "Correct", row = 2, column = 0, command = self.correct)
+        self.addButton(text = "New game", row = 3, column = 1, command = self.reset)
 
     def tooLarge(self):
+        self.upperbound = self.compNum 
+        self.lowerbound = min(self.storeComp)
         self.count += 1
-        self.myNumber = random.randint(0, self.myNumber)
-        guess = "Is the number " + str(self.myNumber)
+        self.compNum = random.randint(0, self.compNum)
+        guess = "Is the number " + str(self.compNum) + "?"
         self.label["text"] = guess
 
     def tooSmall(self):
+        self.lowerbound = self.compNum
+        self.upperbound = max(self.storeComp)
         self.count += 1
-        self.myNumber = random.randint(self.myNumber, 100)
-        guess = "Is the number " + str(self.myNumber)
+        self.compNum = random.randint(self.compNum, 100)
+        guess = "Is the number " + str(self.compNum) + "?"
         self.label["text"] = guess
     
     def correct(self):
         self.label["text"] = "It only took me " + str(self.count) + " tries!"
     
     def reset(self):
-        self.count = 0
-        self.myNumber = random.randint(1, 100)
-        guess = "Is the number " + str(self.myNumber) + "?"
+        self.count = 1
+        self.compNum = random.randint(1, 100)
+        guess = "Is the number " + str(self.compNum) + "?"
         self.label = self.addLabel(text = guess, row = 0, column = 0, sticky = "NSEW", columnspan = 4)
 
 def main():
@@ -42,3 +52,4 @@ def main():
 
 if __name__== "__main__":
     main()
+    
