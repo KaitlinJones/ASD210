e691d9e8446ee6b1a4794eb1b0c229393047bab6
Kaitlin Jones <kaitlinjones@joneskaitlin.com>
2022-01-25 02:09:38 -0800

    need to fix tooLarge tooSmall

--- Week4/countDemo2.py
+++ Week4/countDemo2.py
@@ -8,38 +8,38 @@ class counterDemo(EasyFrame):
         EasyFrame.__init__(self, title = "Guessing Game!")
         self.setSize(300, 300)
         self.count = 1
-        self.lowerbound = 0
-        self.upperbound = 0
+        self.lowerbound = 1
+        self.upperbound = 100
         self.compNum = random.randint(1, 100)
-        self.storeComp = []
         guess = "Is the number " + str(self.compNum) + "?"
         self.label = self.addLabel(text = guess, row = 0, column = 0, sticky = "NSEW", columnspan = 4)
-        for i in self.storeComp:
-            self.storeComp.append(self.compNum)
-
-        self.addButton(text = "Too Large", row = 1, column = 0, command = self.tooLarge)
-        self.addButton(text = "Too Small", row = 1, column = 1, command = self.tooSmall)
+        
+        self.addButton(text = "Too Small", row = 1, column = 0, command = self.tooSmall)
+        self.addButton(text = "Too Large", row = 1, column = 1, command = self.tooLarge)
         self.addButton(text = "Correct", row = 2, column = 0, command = self.correct)
-        self.addButton(text = "New game", row = 3, column = 1, command = self.reset)
+        self.addButton(text = "New game", row = 2, column = 1, command = self.reset)
 
     def tooLarge(self):
-        self.upperbound = self.compNum 
-        self.lowerbound = min(self.storeComp)
         self.count += 1
-        self.compNum = random.randint(0, self.compNum)
+        self.lowerbound = self.compNum + 1
+        self.compNum = (self.upperbound + self.lowerbound) // 2
+        #self.compNum = random.randint(self.lowerbound, self.compNum)
         guess = "Is the number " + str(self.compNum) + "?"
         self.label["text"] = guess
 
     def tooSmall(self):
-        self.lowerbound = self.compNum
-        self.upperbound = max(self.storeComp)
         self.count += 1
-        self.compNum = random.randint(self.compNum, 100)
+        self.upperbound = self.compNum - 1
+        self.compNum = (self.upperbound + self.lowerbound) // 2
+        #self.compNum = random.randint(self.compNum, self.upperbound)
         guess = "Is the number " + str(self.compNum) + "?"
         self.label["text"] = guess
     
     def correct(self):
         self.label["text"] = "It only took me " + str(self.count) + " tries!"
+        self.tooLarge["state"] = "disabled"
+        self.tooSmall["state"] = "disabled"
+        self.correct["state"] = "disabled"
     
     def reset(self):
         self.count = 1
